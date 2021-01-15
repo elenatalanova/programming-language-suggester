4964a2acccb1f7086c835f6a432d1facc3b8121a
Lena Talanova <lena.talanova.ua@gmail.com>
2021-01-15 14:32:37 -0800

    Add an attempt to write javascript function to collect user informatio

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
-  $(#formOne).submit(function(event) {
+  $("#formOne").submit(function(event) {
     event.preventDefault();
-    const personInput = $("#personName").val();
-    const developmentType = $("#question1").val();
-    $("#personName").text(personNameInput);
+    const name = $("#personName").val();
+    const techField = $("#question1").val();
+    const typeOfDevelopment = $("input:radio[name=guestion2]:checked").val();
+    const attentivness = document.querySelector("#question3");
+    const output = document.querySelector(".question3-output");
+    
+    output.textContent = attentivness.range;
+
+    attentivness.addEventListener("input", function() {
+      output.textContent = attentivness.value;
+
+    $(".personName").text(name);
+    $(".question1").radio(techField);
+    $(".question2").text(typeOfDevelopment);
+    $(".question3").range(attentivness);
+
+    $("#output").show()
   });
 });

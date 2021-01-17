fe16b7002095c56541412b3319d26095b5addb5c
Lena Talanova <lena.talanova.ua@gmail.com>
2021-01-17 12:54:37 -0800

    Add some miserable attempts to make my js code work

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    event.preventDefault();
-    const name = $("#personName").val();
-    const techField = $("#question1").val();
-    const typeOfDevelopment = $("input:radio[name=guestion2]:checked").val();
-    const attentivness = document.querySelector("#question3");
-    const output = document.querySelector(".question3-output");
-    
-    output.textContent = attentivness.range;
-
-    attentivness.addEventListener("input", function() {
+  $("form#languageSuggester").click(function(event) {
+    const name = $("input#personName").val();
+    const techField = $("input#question1").val();
+    const typeOfDevelopment = $("input:radio[name=question2]:checked").val();
+    const attentivness = document.querySelector("input#question3");
+    const output = document.querySelector(".range-slider");
+      output.textContent = attentivness.range;
+      attentivness.addEventListener("input", function() {
       output.textContent = attentivness.value;
-
+      
     $(".personName").text(name);
-    $(".question1").radio(techField);
-    $(".question2").text(typeOfDevelopment);
-    $(".question3").range(attentivness);
+    $(".question1").text(techField);
+    $(".typeOfDevelopment").text(typeOfDevelopment);
+    $(".slider").text(attentivness);
 
-    $("#output").show()
+    event.preventDefault();
+    $("#result").show();
   });
 });

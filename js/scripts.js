fe5805a31106cadb7ef3bd6b47addb50c7fca2b2
Lena Talanova <lena.talanova.ua@gmail.com>
2021-01-22 14:38:04 -0800

    Add another class form to html, add javascript, add first attempts to write if/else statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,46 @@
 $(document).ready(function() {
   $("form#languageSuggester").submit(function(event) {
-    const name = $("input#personName").val();
-    const techField = $("input#question1").val();
-    const typeOfDevelopment = $("input:radio[name=question2]:checked").val();
+    event.preventDefault();
+
+    const personName = $("input#personName").val();
+    const question1 = $("input#question1").val();
+    const question2 = $("input#question2").val();
+    const question3 = $("input#question3").val();
+
+    $(".personName").text(personName);
+    $(".question1").text(question1);
+    $(".question2").text(question2);
+    $(".question3").text(question3);
+  });
+  
+    if ( === videoGames) {
+      yourLanguage = Python;
+    } else (techField === other) {
+      your language  = JavaSkript
+    }
+    
+
+
+    
+    /*$(".personName").text(name);
+    $(".question1").text(techField);
+
+    $("#yourLanguage").show();*/
+
+    
+   
+    /*if (techField === videoGames) {
+      yourLanguage = Python;
+    } else (techField === other) {
+      your language  = JavaSkript
+    }*/
+  
+    
+    
+    
+    
+    /*const typeOfDevelopment = $("input:radio[name=question2]:checked").val();
+    const attentivness = 
     const attentivness = document.querySelector("#question3");
     const output = document.querySelector(".question3-output");
 
@@ -11,13 +49,31 @@ $(document).ready(function() {
     question3.addEventListener("input", function() {
      output.textContent = question3.value;
       
+    
+
     $(".personName").text(name);
     $(".question1").text(techField);
     $(".question2").text(typeOfDevelopment);
     $(".question3").text(attentivness);
 
-    event.preventDefault();
-    $("#output").show();
+
     });
   });
-});
+});*/
+
+
+
+
+
+/*const variables = prompt('asdf');
+function qwer(vars) {
+  return 'something';
+}
+alert(qwer(variables));
+
+const variables = prompt('asdf');
+function qwer(vars) {
+  return 'something';
+}
+const result = qwer(variables);
+alert(result);*/

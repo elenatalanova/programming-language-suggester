711ac609c601059d0665476fc13f7a4f34434418
Lena Talanova <lena.talanova.ua@gmail.com>
2021-01-25 14:37:40 -0800

    Add more langueages in the rusult section, if/else statement to all of them and some css styling

--- js/scripts.js
+++ js/scripts.js
@@ -12,12 +12,14 @@ $(document).ready(function() {
     $(".question2").text(question2);
     $(".question3").text(question3);
   
-    if (question1 === "videoGames") {
+    if (question1 === "videoGames", question2 === "databaseAndServers", question3 === "option1" || "option2" || "option3") {
       $(".languageResult1").show();
-    } else {
-      $(".languageResult2").show();
-      $(".result").show();
+    } else if (question1 === "websites", question2 === "gui", question3 === "option1" || "option2" || "option3" || "option4" || "option5") {
+      $(".languageResult2").show(); 
+    } else if (question1 === "windowsApplications", question2 === "databaseAndServers", question3 === "option1" || "option2" || "option3" || "option4" || "option5") {
+      $(".languageResult3").show();
     }
+      $(".result").show();
     $(".quiz").hide();
     });
   });

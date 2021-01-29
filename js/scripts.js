6b192b055959df9836f96d179b23b181066d25ac
Lena Talanova <lena.talanova.ua@gmail.com>
2021-01-28 20:10:12 -0800

    add fixin a bug in jquery by using input[id=question3]:checked add working js skript, add completed project

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,26 @@ $(document).ready(function() {
 
     const personName = $("input#personName").val();
     const question1 = $("select#question1").val();
-    const question2 = $("input#question2").val();
-    const question3 = $("input#question3").val();
+    const question2 = $("input[id=question2]:checked").val();
+    const question3 = $("input[id=question3]:checked").val();
 
     $(".personName").text(personName);
     $(".question1").text(question1);
     $(".question2").text(question2);
     $(".question3").text(question3);
     
-    console.log(question1, question2, question3);
     if (question1 === "videoGames" && question2 === "databaseAndServers" && (question3 === "option1" || question3 === "option2" || question3 === "option3")) {
       $(".languageResult1").show();
 
-      } else if (question1 === "websites" && question2 === "gui" && (question3 === "option1" || question3 === "option2" || question3 === "option3" || question3 === "option4" || question3 === "option5")) {
+    } else if (question1 === "websites" && question2 === "gui") {
       $(".languageResult2").show(); 
 
-      } else if (question1 === "windowsApplications" && question2 === "databaseAndServers" && (question3 === "option1" || question3 === "option2" || question3 === "option3" ||question3 === "option4" || question3 === "option5")) {
+    } else if (question1 === "windowsApplications" && question2 === "databaseAndServers" || question2 === "gui") {
       $(".languageResult3").show();
-    
+
     } else {
-      $(".languageResult1").show();
-    }
+      $(".languageResult1").show();       
+    } 
 
     $(".result").show();
     $(".quiz").hide();
